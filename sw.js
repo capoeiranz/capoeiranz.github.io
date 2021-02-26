eb6075b828c2ca77b416d14bad1f3cb56c243ace
capoeiranz <capoeira@capoeira.org.nz>
2021-02-26 12:54:18 +0000

    Updates

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-6c3ead24af170cdcb87c.js"
   },
   {
-    "url": "app-3b0c5a8f25048b6c8828.js"
+    "url": "app-1cc33d3d014e918d8980.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "10650d0ed6385553e1891f49dad219f3"
+    "revision": "e6c0f2ee1489af52dd4ee4017fa95e60"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5081bfd3b3053fb653b2.js"
@@ -139,7 +139,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-3b0c5a8f25048b6c8828.js`))) {
+  if (!resources || !(await caches.match(`/app-1cc33d3d014e918d8980.js`))) {
     return await fetch(event.request)
   }
 
